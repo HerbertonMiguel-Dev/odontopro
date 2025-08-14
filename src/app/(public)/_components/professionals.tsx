f808701d7f2294640e1b12ffae6356386770b37d
unknown <Administrador@MGIRNSRA048021D.MGI.COLABORAGOV.GOV.BR>
2025-08-14 10:15:15 -0300

    style(professionals): Organiza a identação do código do componente

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -25,7 +25,7 @@ export function Professionals() {
           className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4"
         >
 
-          <Card className="overflow-hidden">
+          <Card className="overflow-hidden p-0">
             <CardContent className="p-0">
               <div>
                 <div className="relative h-48">
@@ -63,11 +63,83 @@ export function Professionals() {
             </CardContent>
           </Card>
 
+          <Card className="overflow-hidden p-0">
+            <CardContent className="p-0">
+              <div>
+                <div className="relative h-48">
+                  <Image
+                    src={fotoImg}
+                    alt="Foto da clinica"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              </div>
 
+              <div className="p-4 space-y-4">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h3 className="font-semibold">
+                      Clinica centro
+                    </h3>
+                    <p className="text-sm text-gray-500">
+                      Rua x, centro, Campo Grande - MS
+                    </p>
+                  </div>
 
+                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
+                </div>
+
+                <Link
+                  href="/clinica/123"
+                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium"
+                >
+                  Agendar horário
+                  <ArrowRight className="ml-2" />
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card className="overflow-hidden p-0">
+            <CardContent className="p-0">
+              <div>
+                <div className="relative h-48">
+                  <Image
+                    src={fotoImg}
+                    alt="Foto da clinica"
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              </div>
+
+              <div className="p-4 space-y-4">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <h3 className="font-semibold">
+                      Clinica centro
+                    </h3>
+                    <p className="text-sm text-gray-500">
+                      Rua x, centro, Campo Grande - MS
+                    </p>
+                  </div>
+
+                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
+                </div>
+
+                <Link
+                  href="/clinica/123"
+                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium"
+                >
+                  Agendar horário
+                  <ArrowRight className="ml-2" />
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
 
         </section>
-
 
       </div>
 
